1b960cdacfc4236aa99b9571c78acac81da7a110
Val Gorodnichev <val.gorodnichev@konghq.com>
2024-04-22 12:24:52 -0600

    fix: add callback example

--- sandbox/components/SampleSpecSelector.vue
+++ sandbox/components/SampleSpecSelector.vue
@@ -1,22 +1,12 @@
 <template>
   <div class="sample-spec-selector">
     <div>Select spec sample:&nbsp;</div>
-    <select
-      ref="specSelector"
-      value=""
-      @change="specSelected"
-    >
-      <option
-        disabled
-        value=""
-      >
+    <select ref="specSelector" value="" @change="specSelected">
+      <option disabled value="">
         Choose...
       </option>
 
-      <option
-        disabled
-        value=""
-      >
+      <option disabled value="">
         ------------------
       </option>
 
@@ -26,17 +16,18 @@
       <option value="/spec-renderer/specs/konnect-api.yaml">
         Konnect Api
       </option>
+      <option value="/spec-renderer/specs/callback.yaml">
+        Callbacks
+      </option>
       <option
-        value="https://raw.githubusercontent.com/digitalocean/openapi/main/specification/DigitalOcean-public.v2.yaml"
-      >
+        value="https://raw.githubusercontent.com/digitalocean/openapi/main/specification/DigitalOcean-public.v2.yaml">
         Digital Ocean
       </option>
       <option value="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml">
         Zoom
       </option>
       <option
-        value="https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/ghes-3.0/ghes-3.0.json"
-      >
+        value="https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/ghes-3.0/ghes-3.0.json">
         GitHub
       </option>
       <option value="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/netlify/openapi.yaml">
@@ -50,20 +41,11 @@
       </option>
     </select>
 
-    <button
-      ref="dropZoneRef"
-      class="dropzone"
-      @click="dropzoneClick()"
-    >
+    <button ref="dropZoneRef" class="dropzone" @click="dropzoneClick()">
       {{ fName }}
     </button>
 
-    <input
-      ref="fileInputRef"
-      style="position: absolute; visibility: hidden;"
-      type="file"
-      @change="finputChange"
-    >
+    <input ref="fileInputRef" style="position: absolute; visibility: hidden;" type="file" @change="finputChange">
   </div>
 </template>
 
