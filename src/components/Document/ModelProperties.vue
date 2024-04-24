d71dd3f898984d7b3f93bc3bef3708ccb088f8b0
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-04-24 23:29:03 +0530

    feat(model-properties): add component to render out object properties

---
+++ src/components/Document/ModelProperties.vue
@@ -0,0 +1,88 @@
+<template>
+  <div
+    v-for="(property, key) in properties"
+    :key="key"
+    class="model-property"
+  >
+    <template v-if="property && typeof property === 'object'">
+      <div
+        class="property-info"
+      >
+        <code>{{ key }}</code>
+        <span
+          class="property-type"
+        >
+          {{ property.type }}
+        </span>
+        <span
+          v-if="required?.includes(key.toString())"
+          class="required-property"
+        >
+          required
+        </span>
+      </div>
+      <p v-if="property.description">
+        {{ property.description }}
+      </p>
+      <p v-if="property.examples">
+        <span>Examples: </span> {{ property.examples }}
+      </p>
+
+      <details v-if="isNestedObj(property)">
+        <summary>Properties of <code>{{ key }}</code></summary>
+        <ModelProperties
+          :properties="property.properties"
+          :required="property.required"
+        />
+      </details>
+    </template>
+
+    <div v-else>
+      <code>{{ key }}</code>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { JSONSchema7 } from 'json-schema'
+
+defineProps({
+  properties: {
+    type: Object as PropType<JSONSchema7['properties']>,
+    required: true,
+  },
+  required: {
+    type: Array as PropType<JSONSchema7['required']>,
+    required: true,
+  },
+})
+
+const isNestedObj = (property: JSONSchema7) => property.type === 'object' && property.properties && Reflect.ownKeys(property.properties).length
+</script>
+
+<style lang="scss" scoped>
+
+.model-property {
+  border-bottom: $kui-border-width-10 solid $kui-color-border;
+  padding: $kui-space-50 0;
+
+  &>:not(:first-child) {
+    margin-top: var(--kui-space-40, $kui-space-40);
+  }
+}
+
+.property-info>:not(:first-child) {
+  margin-left: var(--kui-space-40, $kui-space-40);
+}
+
+.property-type {
+  color: $kui-color-text-neutral-strong;
+  font-size: $kui-font-size-20;
+}
+
+.required-property {
+  color: $kui-color-text-danger;
+  font-size: $kui-font-size-20;
+}
+</style>
