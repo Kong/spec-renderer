f1119ad01e109028586034e6c88de3fabd0b225f
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-04-24 23:29:57 +0530

    feat(model-node): render model info and properties

--- src/components/Document/ModelNode.vue
+++ src/components/Document/ModelNode.vue
@@ -1,17 +1,36 @@
 <template>
-  Model
-  <br>
-  {{ data }}
+  <h3>
+    {{ title }} <code v-if="data.type">{{ data.type }}</code>
+  </h3>
+  <p v-if="data.description">
+    {{ data.description }}
+  </p>
+  <p v-if="data.examples">
+    {{ data.examples }}
+  </p>
+
+  <div v-if="data.properties">
+    <ModelProperties
+      :properties="data.properties"
+      :required="data.required"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
+import ModelProperties from './ModelProperties.vue'
+
 import type { PropType } from 'vue'
-import type { INode } from '@stoplight/types'
+import type { JSONSchema7 } from 'json-schema'
 
 // TODO: type def is missing for model need to add
 defineProps({
   data: {
-    type: Object as PropType<INode>,
+    type: Object as PropType<JSONSchema7>,
+    required: true,
+  },
+  title: {
+    type: String,
     required: true,
   },
 })
