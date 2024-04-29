5c4b0cde0994b9a400d2dab425833f5dc4e86183
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-04-29 23:31:50 +0530

    chore(spec-renderer): add comments for wrapper types

--- src/types/spec-renderer.ts
+++ src/types/spec-renderer.ts
@@ -4,5 +4,10 @@ export interface SpecRendererProps {
   modelValue: string
 }
 
+/**
+ * Wrapper types so that we don't import/use types from a 3rd party library, like openapi3-ts, directly in our code.
+ * This way it'll be easy to replace out this library with some other library, or even our own implementation,
+ * without requiring major refactoring.
+ */
 export interface SchemaObject extends OAS31SchemaObject {}
 export interface ReferenceObject extends OAS31ReferenceObject {}
