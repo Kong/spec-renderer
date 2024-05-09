11a64e3ca898e65243713b6d2bd07ddd36be6362
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-05-09 17:27:31 +0530

    feat(http-service): render some basic data for overview page

--- src/components/spec-document/HttpService.vue
+++ src/components/spec-document/HttpService.vue
@@ -1,8 +1,36 @@
 <template>
   <div>
-    HttpService
-    <br>
-    {{ data }}
+    <h3>{{ data.name }}</h3>
+    <p>v{{ data.version }}</p>
+    <div>
+      <h4>Servers</h4>
+      <ul>
+        <li
+          v-for="server in data.servers"
+          :key="server.url"
+        >
+          {{ server.name }} {{ server.description }}:
+          <a
+            :href="server.url"
+            rel="noopener noreferrer"
+            target="_blank"
+          >{{ server.url }}</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h4>Security</h4>
+      <div
+        v-for="scheme in data.securitySchemes"
+        :key="scheme.toString()"
+      >
+        {{ scheme.key }}
+        <p>
+          {{ scheme.description }}
+        </p>
+      </div>
+    </div>
+    <p>v{{ data.description }}</p>
   </div>
 </template>
 
