a8d35608247f394cecf1db6309aab388fb5b2bb3
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-05-09 23:31:11 +0530

    feat(http-operation): render basic info of endpoint request

--- src/components/spec-document/HttpOperation.vue
+++ src/components/spec-document/HttpOperation.vue
@@ -1,14 +1,42 @@
 <template>
-  <div>
-    IHttpOperation
-    <br>
-    {{ data }}
+  <div :data-testid="`http-operation-${data.id}`">
+    <section>
+      <h3>{{ data.summary }}</h3>
+      <p>{{ data.description }}</p>
+    </section>
+
+    <section v-if="data.request">
+      <h4>Request</h4>
+      <div v-if="data.request.query?.length">
+        <h5>Query Parameters</h5>
+        <div>
+          <div
+            v-for="queryParam in data.request.query"
+            :key="queryParam.id"
+          >
+            <PropertyInfo
+              :format="queryParam.schema?.format"
+              :property-type="queryParam.schema?.type"
+              :required-fields="queryParam.schema?.required"
+              :title="queryParam.name"
+            />
+
+            <PropertyDescription
+              v-if="queryParam.description"
+              :description="queryParam.description"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import type { IHttpOperation } from '@stoplight/types'
+import PropertyInfo from './property-fields/PropertyInfo.vue'
+import PropertyDescription from './property-fields/PropertyDescription.vue'
 
 defineProps({
   data: {
