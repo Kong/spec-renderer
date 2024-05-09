7c7e19879bb60143c6280d7e2cc85fbe06711615
Val Gorodnichev <val.gorodnichev@konghq.com>
2024-05-09 09:06:28 -0600

    chore: tooling for design token autocomple

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -15,7 +15,14 @@
   </div>
 </template>
 
+<style lang="scss">
+.spec-render-doc {
+  --kui-color-background-transparent: lightgreen;
+}
+</style>
+
 <style lang="scss" scoped>
+
 .sandbox-container {
   font-family: $kui-font-family-text;
 }
