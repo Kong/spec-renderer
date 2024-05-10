2e3af8bd5570ac7fcb8aba3e773bb8c53de1db2d
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-05-10 20:27:15 +0530

    feat: add query param list component

---
+++ src/components/document/endpoint/QueryParamList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="query-param-list">
+    <h5>Query Parameters</h5>
+    <div
+      v-for="queryParam in queryParamList"
+      :key="queryParam.id"
+      class="query-param-list-item"
+    >
+      <PropertyInfo
+        :format="queryParam.schema?.format"
+        :property-type="queryParam.schema?.type"
+        :required-fields="queryParam.schema?.required"
+        :title="queryParam.name"
+      />
+
+      <PropertyDescription
+        v-if="queryParam.description"
+        :description="queryParam.description"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { IHttpQueryParam } from '@stoplight/types'
+import PropertyInfo from '@/components/document/property-fields/PropertyInfo.vue'
+import PropertyDescription from '@/components/document/property-fields/PropertyDescription.vue'
+
+defineProps({
+  queryParamList: {
+    type: Array as PropType<Array<IHttpQueryParam>>,
+    required: true,
+  },
+})
+</script>
--- src/components/spec-document/HttpOperation.vue
+++ src/components/spec-document/HttpOperation.vue
@@ -7,27 +7,10 @@
 
     <section v-if="data.request">
       <h4>Request</h4>
-      <div v-if="data.request.query?.length">
-        <h5>Query Parameters</h5>
-        <div>
-          <div
-            v-for="queryParam in data.request.query"
-            :key="queryParam.id"
-          >
-            <PropertyInfo
-              :format="queryParam.schema?.format"
-              :property-type="queryParam.schema?.type"
-              :required-fields="queryParam.schema?.required"
-              :title="queryParam.name"
-            />
-
-            <PropertyDescription
-              v-if="queryParam.description"
-              :description="queryParam.description"
-            />
-          </div>
-        </div>
-      </div>
+      <QueryParamList
+        v-if="data.request.query?.length"
+        :query-param-list="data.request.query"
+      />
     </section>
   </div>
 </template>
@@ -35,8 +18,7 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import type { IHttpOperation } from '@stoplight/types'
-import PropertyInfo from './property-fields/PropertyInfo.vue'
-import PropertyDescription from './property-fields/PropertyDescription.vue'
+import QueryParamList from '@/components/document/endpoint/QueryParamList.vue'
 
 defineProps({
   data: {
