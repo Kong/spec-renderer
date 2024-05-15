6f1e1685cb60f94537990f9ad7a490f47c2ba32f
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-05-15 19:52:33 +0530

    refactor(property-any-of): update component to be JSON Schema compatible

--- src/components/spec-document/property-fields/PropertyAnyOf.vue
+++ src/components/spec-document/property-fields/PropertyAnyOf.vue
@@ -1,33 +1,29 @@
 <template>
   <div data-testid="property-field-any-of">
     <div>anyOf: </div>
-    <template
+    <ModelProperty
       v-for="(anyOfObject, index) in filteredAnyOfList"
       :key="index"
-    >
-      <ModelProperty
-        v-if="isValidSchemaObject(anyOfObject)"
-        :property="anyOfObject"
-        :property-name="inheritedPropertyName(index, anyOfObject.title)"
-        :required-fields="anyOfObject.required"
-      />
-    </template>
+      :property="anyOfObject"
+      :property-name="inheritedPropertyName(index, anyOfObject.title)"
+      :required-fields="anyOfObject.required"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import type { PropType } from 'vue'
-import type { ReferenceObject, SchemaObject } from '@/types'
+import type { SchemaObject } from '@/types'
 import ModelProperty from '../ModelProperty.vue'
-import { isValidSchemaObject, inheritedPropertyName } from '@/utils'
+import { inheritedPropertyName, filterSchemaObjectArray } from '@/utils'
 
 const props = defineProps({
   anyOfList: {
-    type: Array as PropType<Array<SchemaObject | ReferenceObject>>,
+    type: Array as PropType<SchemaObject['anyOf']>,
     required: true,
   },
 })
 
-const filteredAnyOfList = computed(() => props.anyOfList.filter(item => Boolean(item)))
+const filteredAnyOfList = computed(() => filterSchemaObjectArray(props.anyOfList))
 </script>
