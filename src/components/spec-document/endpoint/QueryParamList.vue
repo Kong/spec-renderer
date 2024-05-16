7a6655038a3d7539b4e05867452791c8a905e2b3
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-05-16 17:53:06 +0530

    feat(query-param-list): use ModelProperty component for rendering query param schema

--- src/components/spec-document/endpoint/QueryParamList.vue
+++ src/components/spec-document/endpoint/QueryParamList.vue
@@ -6,16 +6,11 @@
       :key="queryParam.id"
       class="query-param-list-item"
     >
-      <PropertyInfo
-        :format="queryParam.schema?.format"
-        :property-type="queryParam.schema?.type"
-        :required-fields="queryParam.schema?.required"
-        :title="queryParam.name"
-      />
-
-      <PropertyDescription
-        v-if="queryParam.description"
-        :description="queryParam.description"
+      <ModelProperty
+        v-if="queryParam.schema"
+        :property="{...queryParam.schema, ...populateQueryParamProperty(queryParam) }"
+        :property-name="queryParam.name"
+        :required-fields="queryParamItemRequiredFields(queryParam)"
       />
     </div>
   </div>
@@ -24,8 +19,8 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import type { IHttpQueryParam } from '@stoplight/types'
-import PropertyInfo from '../property-fields/PropertyInfo.vue'
-import PropertyDescription from '../property-fields/PropertyDescription.vue'
+import ModelProperty from '../ModelProperty.vue'
+import type { SchemaObject } from '@/types'
 
 defineProps({
   queryParamList: {
@@ -33,4 +28,29 @@ defineProps({
     required: true,
   },
 })
+
+// some fields might not be available in the quer params schema, so we populate them from the query param itself
+const populateQueryParamProperty = (queryParam: IHttpQueryParam) => {
+  if (!queryParam.schema) return null
+
+  const property: SchemaObject = {}
+  if (queryParam.description && !property?.description) {
+    // sometimes description for query param is not present in the schema
+    // and is instead added to the query param itself
+    property.description = queryParam.description
+  }
+
+  return property
+}
+
+/**
+ * If the queryParam's schema has required fields, we use it.
+ * Else, we use the `required` boolean field of the queryParam itself to determine if the field is required.
+ */
+const queryParamItemRequiredFields = (queryParam: IHttpQueryParam) => {
+  if (queryParam.schema?.required) {
+    return queryParam.schema?.required
+  }
+  return queryParam.required ? [queryParam.name] : []
+}
 </script>
