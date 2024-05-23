92e46c1a83f09ed337fb34ad89802cf30784cb4c
ValeryG <vvalggor@gmail.com>
2024-05-23 13:49:37 -0600

    Feat/khcp 12067 try it authentication panel (#80)

---
+++ src/components/spec-document/HttpOperation.spec.ts
@@ -0,0 +1,65 @@
+import { ref } from 'vue'
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import HttpOperation from './HttpOperation.vue'
+import type { IHttpOperation, IHttpService, IServer } from '@stoplight/types'
+
+describe('<HttpOperation />', () => {
+  describe('TryIt section', () => {
+
+    it('TryIt is shown for operation', () => {
+      const wrapper = mount(HttpOperation, {
+        props: {
+          data: <IHttpOperation>{
+            id: '123',
+          },
+          overviewData: <IHttpService>{
+            servers: <Array<IServer>>[{
+              id: 'sample-server-id',
+              url: 'https://stoplight.io/api',
+              description: 'sample description',
+            }],
+          },
+        },
+      })
+      expect(wrapper.findTestId('tryit-123').exists()).toBe(true)
+    })
+
+    it('TryIt is not rendered when hideTryIt is true', () => {
+      const wrapper = mount(HttpOperation, {
+        props: {
+          data: <IHttpOperation>{
+            id: '123',
+          },
+          overviewData: <IHttpService>{
+            servers: <Array<IServer>>[{
+              id: 'sample-server-id',
+              url: 'https://stoplight.io/api',
+              description: 'sample description',
+            }],
+          },
+        },
+        global: {
+          provide: {
+            'hide-tryit': ref(true),
+          },
+        },
+
+      })
+      expect(wrapper.findTestId('tryit-123').exists()).toBe(false)
+    })
+
+    it('TryIt is not rendered when server list is not defined in the spec', () => {
+      const wrapper = mount(HttpOperation, {
+        props: {
+          data: <IHttpOperation>{
+            id: '123',
+          },
+          overviewData: <IHttpService>{
+          },
+        },
+      })
+      expect(wrapper.findTestId('tryit-123').exists()).toBe(false)
+    })
+  })
+})
