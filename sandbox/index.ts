3ba8452ab3459a12f9801e4fe27f3e061169778d
Maksym Portianoi <portimaksym@gmail.com>
2024-05-30 11:14:33 -0400

    fix(spec-renderer-toc): hide endpoints title when schemas are hidden [KHCP-12097] (#90)
    * fix(spec-renderer-toc): misc changes [KHCP-12097]
    
    * test(spec-renderer-toc): add test scenario [KHCP-12097]
    
    * test(spec-renderer-toc): fix test [KHCP-12097]
    
    * chore(sandbox): add kongponents [KHCP-12097]
    
    * fix(spec-renderer-toc): minor fix [KHCP-12097]
    
    * fix(spec-renderer-toc): minor fix [KHCP-12097]
    
    * fix(spec-renderer-toc): hide endpoints title [KHCP-12097]
    
    * fix: stylelint:fix
    
    * fix(toc): address pr feedback [KHCP-12097]
    
    * fix(toc): minor fix [KHCP-12097]

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/scss/_base.scss'
 
 const app = createApp(App)
--- src/stoplight/elements/components/API/utils.ts
+++ src/stoplight/elements/components/API/utils.ts
@@ -97,6 +97,7 @@ export const computeAPITree = (serviceNode: ServiceNode, config: ComputeAPITreeC
     tree.push({
       title: 'Endpoints',
       items: [],
+      hideTitle: mergedConfig.hideSchemas,
     })
 
     const { groups, ungrouped } = computeTagGroups<OperationNode>(serviceNode, NodeType.HttpOperation)
