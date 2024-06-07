9346291c362a9f069cd530823488ff5bca321639
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-06-07 20:22:22 +0530

    chore(circular-remover): remove util as not used anymore

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 // Only export external utilities
-export * from './circular-remover'
 export * from './schema-parser'
 export * from './schema-model'
 export * from './get-offset-top-relative-to-parent'
