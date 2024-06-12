b4a9c87eca472eece49e92b9117e6e3968c36834
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-06-12 13:21:42 +0530

    feat: add copy button and typography to server-endpoint component

---
+++ src/components/common/CopyButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    ref="copyButton"
+    class="copy-button"
+    @click="copyCode"
+  >
+    <CopyIcon
+      :color="iconColor"
+      :size="iconsSize"
+    />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { CopyIcon } from '@kong/icons'
+import { copyTextToClipboard } from '@/utils/copyTextToClipboard'
+
+const props = defineProps({
+  content:{
+    required: true,
+    type: String,
+  },
+  iconsSize: {
+    type: String,
+    default: '16px',
+  },
+  iconColor: {
+    type: String,
+    default: 'currentColor',
+  },
+})
+
+const copyButton = ref<HTMLButtonElement>()
+
+async function copyCode(): Promise<void> {
+  // todo: show a message if copied successfully
+  await copyTextToClipboard(props.content)
+}
+</script>
+
+<style lang="scss" scoped>
+.copy-button {
+  @include default-button-reset;
+}
+</style>
--- src/components/spec-document/endpoint/ServerEndpoint.vue
+++ src/components/spec-document/endpoint/ServerEndpoint.vue
@@ -9,35 +9,24 @@
         size="large"
       />
       <div
-        class="server-endpoint-url"
+        class="server-endpoint-url-with-path"
         data-testid="server-endpoint-url-with-path"
       >
-        <span>{{ selectedServerUrl }}</span><span>{{ path }}</span>
+        <span class="selected-server-url">{{ selectedServerUrl }}</span>
+        <span class="endpoint-path">{{ path }}</span>
+        <ChevronDownIcon size="20px" />
       </div>
     </div>
-    <div class="right">
-      <select
-        data-testid="endpoint-server-select"
-        name="endpoint-server"
-        @change="changeEndpointServer"
-      >
-        <option
-          v-for="serverURL in serverUrlList"
-          :key="serverURL"
-          :data-testid="`server-endpoint-option-${serverURL}`"
-          :selected="serverURL === selectedServerUrl"
-          :value="serverURL"
-        >
-          {{ serverURL }}
-        </option>
-      </select>
-    </div>
+
+    <CopyButton :content="selectedServerUrl+path" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
+import { ChevronDownIcon } from '@kong/icons'
 import MethodBadge from '@/components/common/MethodBadge.vue'
+import CopyButton from '@/components/common/CopyButton.vue'
 
 defineProps({
   method: {
@@ -76,10 +65,27 @@ function changeEndpointServer(event: Event) {
   justify-content: space-between;
   padding: var(--kui-space-50, $kui-space-50);
 
-  &>.current-endpoint {
+  >.current-endpoint {
     align-items: center;
     display: flex;
     gap: var(--kui-space-20, $kui-space-20);
+
+    .server-endpoint-url-with-path {
+      align-items: center;
+      display: flex;
+      font-family: var(--kui-font-family-code, $kui-font-family-code);
+      font-size: var(--kui-font-size-30, $kui-font-size-30);
+      gap: var(--kui-space-20, $kui-space-20);
+      line-height: var(--kui-line-height-40, $kui-line-height-40);
+
+      .selected-server-url {
+        color: var(--kui-color-text-neutral-strong, $kui-color-text-neutral-strong);
+      }
+      .endpoint-path {
+        color: var(--kui-color-text, $kui-color-text);
+        font-weight: var(--kui-font-weight-semibold, $kui-font-weight-semibold);
+      }
+    }
   }
 }
 </style>
