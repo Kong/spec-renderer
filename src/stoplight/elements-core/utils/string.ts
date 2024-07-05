d1e1e934377a6c331a9bbe66ce880153e81f09fa
Maksym Portianoi <portimaksym@gmail.com>
2024-07-05 11:53:48 -0400

    fix(*): replace lodash with lodash-es [KHCP-11622] (#181)

--- src/stoplight/elements-core/utils/string.ts
+++ src/stoplight/elements-core/utils/string.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck external file
-import { curry } from 'lodash'
+import { curry } from 'lodash-es'
 
 export const caseInsensitivelyEquals = curry((a: string, b: string) => a.toUpperCase() === b.toUpperCase())
 
