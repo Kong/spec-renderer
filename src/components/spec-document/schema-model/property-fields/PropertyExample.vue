29a9b22069abe23432ba0105b391443a31f01a8e
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-07-11 22:25:24 +0530

    feat(property-example): stringify example array and objects

--- src/components/spec-document/schema-model/property-fields/PropertyExample.vue
+++ src/components/spec-document/schema-model/property-fields/PropertyExample.vue
@@ -4,17 +4,35 @@
     data-testid="property-field-example"
   >
     <span class="field-title">Example:</span>
-    <span class="property-field-example-value">{{ example }}</span>
+    <span class="property-field-example-value">{{ stringifiedExample }}</span>
   </p>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { computed } from 'vue'
+
+const props = defineProps({
   example: {
     // allow any type since typeof example is unknown
     type: null,
     required: true,
   },
+})
+
+const stringifiedExample = computed(() => {
+  let stringToRender = ''
+  if (Array.isArray(props.example)) {
+    // can't do a simple Array.toString because items of type Object are
+    // converted to [Object Object], hence we need to stringify object items
+    props.example.forEach((example, index) => {
+      stringToRender +=
+        typeof(example) === 'object' ? JSON.stringify(example) : example +
+        `${index < props.example.length - 1 ? ',' : ''}`
+    })
+  } else if (typeof props.example === 'object') {
+    stringToRender = JSON.stringify(props.example)
+  }
+  return stringToRender || props.example
 })
 </script>
 
