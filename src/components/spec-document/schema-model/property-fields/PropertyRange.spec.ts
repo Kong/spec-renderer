93e48c5b752c0e03901cf32bd95baf96f97d61eb
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-08-01 21:56:52 +0530

    test(property-range): update tests

--- src/components/spec-document/schema-model/property-fields/PropertyRange.spec.ts
+++ src/components/spec-document/schema-model/property-fields/PropertyRange.spec.ts
@@ -10,25 +10,81 @@ describe('<PropertyRange />', () => {
         min: 10,
       },
     })
+
     expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
-    expect(wrapper.text()).toEqual('>= 100<= 10')
+    expect(wrapper.text()).toEqual('>= 10<= 100')
   })
+
   it('renders with only max', () => {
     const wrapper = shallowMount(PropertyRange, {
       props: {
         max: 100,
       },
     })
+
     expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
-    expect(wrapper.text()).toEqual('>= 100')
+    expect(wrapper.text()).toEqual('<= 100')
   })
-  it('renders with both max and min', () => {
+
+  it('renders with only min', () => {
     const wrapper = shallowMount(PropertyRange, {
       props: {
         min: 10,
       },
     })
+
     expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
-    expect(wrapper.text()).toEqual('<= 10')
+    expect(wrapper.text()).toEqual('>= 10')
+  })
+
+  it('renders with exclusiveMinimum and exclusiveMaximum as booleans', () => {
+    const wrapper = shallowMount(PropertyRange, {
+      props: {
+        min: 10,
+        exclusiveMinimum: true,
+        max: 100,
+        exclusiveMaximum: true,
+      },
+    })
+
+    expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
+    expect(wrapper.text()).toEqual('> 10< 100')
+  })
+
+  it('renders with exclusiveMinimum and exclusiveMaximum as numbers', () => {
+    const wrapper = shallowMount(PropertyRange, {
+      props: {
+        min: 10,
+        exclusiveMinimum: 11,
+        max: 100,
+        exclusiveMaximum: 99,
+      },
+    })
+
+    expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
+    expect(wrapper.text()).toEqual('>= 10<= 100> 11< 99')
+  })
+
+  it('renders with multipleOf', () => {
+    const wrapper = shallowMount(PropertyRange, {
+      props: {
+        multipleOf: 5,
+      },
+    })
+
+    expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
+    expect(wrapper.text()).toEqual('x 5')
+  })
+
+  it('renders with minItems and maxItems', () => {
+    const wrapper = shallowMount(PropertyRange, {
+      props: {
+        minItems: 2,
+        maxItems: 5,
+      },
+    })
+
+    expect(wrapper.findTestId('property-field-range').exists()).toBe(true)
+    expect(wrapper.text()).toEqual('>= 2 items<= 5 items')
   })
 })
