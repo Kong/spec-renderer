2f085209bb41b6986a4fe0503ba551f8f6f4a97c
Vaibhav Raj Singh <vaibhav.rajsingh@konghq.com>
2024-08-06 22:31:06 +0530

    fix: markdown fixes [KHCP-12854] (#251)
    * fix(try-it-auth): add check for empty security group array
    
    * feat(markdown): enable linkify and allow img tag in markdown parsing
    
    * feat(variables): make secondary column width relative
    
    * Revert "feat(variables): make secondary column width relative"
    
    This reverts commit c30122c8fc911a1dd3af3fb09a8ecbebd13c8ed7.
    
    * fix(markdown-renderer): store rendered markdown as computed property to rpevent infinite renders
    
    * fix(markdown-renderer): convert render to a method to prevent stale data
    
    * chore: use default font-size for markdown content

--- src/components/spec-document/endpoint/BodyContentList.vue
+++ src/components/spec-document/endpoint/BodyContentList.vue
@@ -70,8 +70,6 @@ function parseSchema(schema: SchemaObject) {
 
   .body-content-list-description {
     color: var(--kui-color-text-neutral-stronger, $kui-color-text-neutral-stronger);
-    font-size: var(--kui-font-size-30, $kui-font-size-30);
-    line-height: var(--kui-line-height-30, $kui-line-height-30);
     margin-bottom: var(--kui-space-40, $kui-space-40);
   }
 
