8b82162c5fc139cddc9491f7875876d60ebdf09d
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-08-21 18:49:46 +0530

    feat(property-default): add component to show default values

---
+++ src/components/spec-document/schema-model/property-fields/PropertyDefault.vue
@@ -0,0 +1,30 @@
+<template>
+  <p
+    class="property-field-default"
+    data-testid="property-field-default"
+  >
+    <span class="field-title">Default:</span>
+    <span class="property-field-default-value">
+      {{ defaultValue }}
+    </span>
+  </p>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  defaultValue: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.property-field-default {
+  @include model-property-additional-field;
+
+  .property-field-default-value {
+    @include model-property-field-value;
+  }
+}
+</style>
