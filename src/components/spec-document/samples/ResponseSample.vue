4356d35c5872669a22e60285f0eaf0b84e1e1514
Vaibhav <vaibhav.rajsingh@konghq.com>
2024-08-27 22:22:53 +0530

    feat(response-type-select): add response-type-select to response sample section

--- src/components/spec-document/samples/ResponseSample.vue
+++ src/components/spec-document/samples/ResponseSample.vue
@@ -3,15 +3,8 @@
     class="response-sample"
     :schema-example-json="activeResponseSample"
   >
-    <div class="response-sample-header-left">
-      <ResponseCodeDot
-        :response-code="getResponseCodeKey(responseCode)"
-        size="large"
-      />
-      {{ responseCode }}
-    </div>
+    <slot />
     <div class="response-sample-header-right">
-      <span class="content-type-label">{{ contentType }}</span>
       <SelectDropdown
         v-if="exampleSelectList && exampleSelectList.length > 1"
         id="response-sample-select"
@@ -31,9 +24,8 @@
 import { ref, computed } from 'vue'
 import type { PropType } from 'vue'
 import type { IMediaTypeContent } from '@stoplight/types'
-import { getResponseCodeKey, getSampleBody } from '@/utils'
+import { getSampleBody } from '@/utils'
 import SchemaExample from '@/components/common/SchemaExample.vue'
-import ResponseCodeDot from '@/components/common/ResponseCodeDot.vue'
 import CopyButton from '@/components/common/CopyButton.vue'
 import SelectDropdown from '@/components/common/SelectDropdown.vue'
 import type { SelectItem } from '@/types'
@@ -80,28 +72,11 @@ const exampleSelectList = computed((): Array<SelectItem> => {
 
 <style lang="scss" scoped>
 .response-sample {
-  .response-sample-header-left {
-    align-items: center;
-    display: inline-flex;
-    font-size: var(--kui-font-size-30, $kui-font-size-30);
-    font-weight: var(--kui-font-weight-semibold, kui-font-weight-semibold);
-    gap: var(--kui-space-40, $kui-space-40);
-    line-height: var(--kui-line-height-40, $kui-line-height-40);
-  }
   .response-sample-header-right {
     align-items: center;
     display: inline-flex;
     gap: var(--kui-space-50, $kui-space-50);
-    .content-type-label {
-      border: var(--kui-border-width-10, $kui-border-width-10) solid var(--kui-color-border, $kui-color-border);
-      border-radius: var(--kui-border-radius-20, $kui-border-radius-20);
-      color: var(--kui-color-text-neutral-strong, $kui-color-text-neutral-strong);
-      font-family: var(--kui-font-family-code, $kui-font-family-code);
-      font-size: var(--kui-font-size-20, $kui-font-size-20);
-      font-weight: var(--kui-font-weight-regular, $kui-font-weight-regular);
-      line-height: var(--kui-line-height-20, $kui-line-height-20);
-      padding: var(--kui-space-10, $kui-space-10) var(--kui-space-30, $kui-space-30);
-    }
+
     .response-sample-selector {
       line-height: var(--kui-line-height-30, $kui-line-height-30);
       :deep(.trigger-button) {
