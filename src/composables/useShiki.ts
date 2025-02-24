e6b73540f29ec309e9db1ed690d7924d36ebd7cd
Vaibhav Raj Singh <vaibhav.rajsingh@konghq.com>
2025-02-24 23:49:13 +0530

    chore(deps): upgrade to shiki v2 (#521)

--- src/composables/useShiki.ts
+++ src/composables/useShiki.ts
@@ -1,6 +1,7 @@
 import type { Ref, DeepReadonly } from 'vue'
 import { ref, readonly } from 'vue'
 import { createHighlighterCore } from 'shiki/core'
+import { createOnigurumaEngine } from 'shiki/engine/oniguruma'
 import type { HighlighterCore } from 'shiki/core'
 
 
@@ -32,7 +33,7 @@ export default function useShiki(): {
         import('shiki/langs/fish.mjs'),
       ],
       // Important: If running in SSR, the host application must have a `shiki/onig.wasm` file available at the root of the assets. If in new Konnect Dev Portal, this is already handled.
-      loadWasm: () => {
+      engine: createOnigurumaEngine(() => {
         // @ts-ignore - Checking for SSR
         if (!import.meta.server) {
           // @ts-ignore - in client, use the wasm loader
@@ -41,7 +42,7 @@ export default function useShiki(): {
           // @ts-ignore - Use externalized import to support rendering via SSR. The Vite Ignore comment is to prevent Vite from trying to resolve the import in the standalone spec renderer
           return import(/* @vite-ignore */ 'shiki/onig.wasm')
         }
-      },
+      }),
     })
   }
 
