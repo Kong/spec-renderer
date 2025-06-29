0431902b3c97c53c53c83915b2adb9e7d7f65baa
Adam DeHaven <adam.dehaven@konghq.com>
2025-06-29 14:51:45 -0400

    fix: add navigation type

--- src/types/nitro.ts
+++ src/types/nitro.ts
@@ -1,20 +1,21 @@
 import type { SpecRendererProps } from './spec-renderer'
 
 export interface SpecRendererNitroConfig extends Pick<SpecRendererProps,
-  'basePath' |
-  'currentPath' |
-  Required<'specUrl'> |
-  'controlAddressBar' |
-  'hideSchemas' |
-  'hideInternal' |
-  'hideDeprecated' |
-  'hideTryIt' |
-  'hideInsomniaTryIt' |
-  'withCredentials' |
-  'allowContentScrolling' |
-  'markdownStyles' |
-  'allowCustomServerUrl' |
-  'hideNavigationButtons' |
-  'hideDownloadButton' |
-  'maxExpandedDepth'
+'basePath' |
+'currentPath' |
+Required<'specUrl'> |
+'controlAddressBar' |
+'navigationType' |
+'hideSchemas' |
+'hideInternal' |
+'hideDeprecated' |
+'hideTryIt' |
+'hideInsomniaTryIt' |
+'withCredentials' |
+'allowContentScrolling' |
+'markdownStyles' |
+'allowCustomServerUrl' |
+'hideNavigationButtons' |
+'hideDownloadButton' |
+'maxExpandedDepth'
 > { }
