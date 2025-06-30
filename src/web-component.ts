6b507450f9395e4239b10cd2fc186432d4172adf
Adam DeHaven <adam.dehaven@konghq.com>
2025-06-30 09:39:52 -0400

    refactor: export function

--- src/web-component.ts
+++ src/web-component.ts
@@ -8,34 +8,36 @@ import SchemaRenderer from './components/extra-renderers/SchemaRenderer.vue'
 export * from './index'
 
 // Exports a function that registers all custom elements as native web components
-// export function registerKongSpecRenderer(): void {
+export function registerKongSpecRenderer(): void {
 
-if (!customElements.get('kong-spec-renderer')) {
-  const specRendererCustomElement = defineCustomElement(SpecRenderer)
-  customElements.define('kong-spec-renderer', specRendererCustomElement)
+  if (!customElements.get('kong-spec-renderer')) {
+    const specRendererCustomElement = defineCustomElement(SpecRenderer)
+    customElements.define('kong-spec-renderer', specRendererCustomElement)
+  }
+
+  if (!customElements.get('kong-spec-renderer-toc')) {
+    const specRendererTocCustomElement = defineCustomElement(SpecRendererToc)
+    customElements.define('kong-spec-renderer-toc', specRendererTocCustomElement)
+  }
+
+  if (!customElements.get('kong-spec-renderer-document')) {
+    const specRendererDocumentCustomElement = defineCustomElement(SpecDocument)
+    customElements.define('kong-spec-renderer-document', specRendererDocumentCustomElement)
+  }
+
+  if (!customElements.get('kong-schema-renderer')) {
+    const schemaRendererCustomElement = defineCustomElement(SchemaRenderer)
+    customElements.define('kong-schema-renderer', schemaRendererCustomElement)
+  }
+
+  console.log('elements registered')
 }
 
-if (!customElements.get('kong-spec-renderer-toc')) {
-  const specRendererTocCustomElement = defineCustomElement(SpecRendererToc)
-  customElements.define('kong-spec-renderer-toc', specRendererTocCustomElement)
+// Auto-register the function to the window object
+if (typeof window !== 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  window.registerKongSpecRenderer = registerKongSpecRenderer
 }
 
-if (!customElements.get('kong-spec-renderer-document')) {
-  const specRendererDocumentCustomElement = defineCustomElement(SpecDocument)
-  customElements.define('kong-spec-renderer-document', specRendererDocumentCustomElement)
-}
-
-if (!customElements.get('kong-schema-renderer')) {
-  const schemaRendererCustomElement = defineCustomElement(SchemaRenderer)
-  customElements.define('kong-schema-renderer', schemaRendererCustomElement)
-}
-
-console.log('elements registered')
-// }
-
-// // Auto-register the function to the window object
-// if (typeof window !== 'undefined') {
-//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//   // @ts-ignore
-//   window.registerKongSpecRenderer = registerKongSpecRenderer
-// }
+registerKongSpecRenderer()
