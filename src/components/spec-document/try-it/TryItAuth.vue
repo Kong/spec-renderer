c303106b28ee2528cc1d41a7a1c04310431412fc
ValeryG <val.gorodnichev@konghq.com>
2025-07-31 08:04:17 -0600

    fix: remove console.log

--- src/components/spec-document/try-it/TryItAuth.vue
+++ src/components/spec-document/try-it/TryItAuth.vue
@@ -183,7 +183,6 @@ const emit = defineEmits<{
 const { activeSecurityScheme, authHeadersMap, authQueryMap, authInputs } = composables.useAuth()
 
 const securitySchemeGroupList = inject<ComputedRef<SecuritySchemeGroup[]>>('security-scheme-group-list', computed(() => []))
-console.log('securitySchemeGroupList', securitySchemeGroupList.value, props.data.security)
 /**
  * Extracts the list of select-items for the security scheme group selector.
 */
