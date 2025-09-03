2951449a3aa4032a3b3e1eda5a7e99e6cb0b85c2
Vaibhav Raj Singh <vaibhav.rajsingh@konghq.com>
2025-09-03 22:03:06 +0530

    feat(spec-document): add support for endpoint tags [TDX-6042] (#672)
    * feat(spec-document): add support for endpoint tags [TDX-6042]
    
    * feat(operation-tag): add id to endpoint tag

---
+++ src/components/spec-document/HttpOperationTag.vue
@@ -0,0 +1,24 @@
+<template>
+  <PageHeader
+    :id="`tag-${kebabCase(name)}`"
+    class="http-operation-header"
+    :description="description"
+    :title="name"
+  />
+</template>
+
+<script setup lang="ts">
+import PageHeader from '../common/PageHeader.vue'
+import { kebabCase } from '@/utils'
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+})
+</script>
--- src/components/spec-document/SpecDocument.spec.ts
+++ src/components/spec-document/SpecDocument.spec.ts
@@ -147,4 +147,121 @@ describe('<SpecDocument />', () => {
     expect(wrapper.findTestId('server-url-get-http://localhost:8001/coffee/hot').exists()).toBe(true)
   })
 
+  it('should render operation tags', () => {
+    const wrapper = mount(SpecDocument, {
+      props: {
+        document: {
+          'type': 'http_service',
+          'uri': '/',
+          'name': 'Spec with tags',
+          'data': {
+            'id': 'undefined',
+            'version': '0.1.0',
+            'name': 'Spec with tags',
+            'description': 'This is a specification document with tags for organizing endpoints.',
+            'extensions': {},
+            'infoExtensions': {},
+            'tags': [
+              {
+                'id': '85d5b4a0a594c',
+                'name': 'First Tag',
+                'description': 'This is the first tag',
+              },
+            ],
+            'servers': [
+              {
+                'id': '1c08f959b65b5',
+                'url': 'https://konghq.com',
+                'name': 'Spec with tags',
+              },
+            ],
+          },
+          'tags': [
+            'First Tag',
+          ],
+          'children': [
+            {
+              'type': 'http_operation',
+              'uri': '/operations/getAlpha',
+              'data': {
+                'id': '2e9146a964a8d',
+                'method': 'get',
+                'path': '/alpha',
+                'tags': [
+                  {
+                    'id': '85d5b4a0a594c',
+                    'name': 'First Tag',
+                  },
+                ],
+                'extensions': {},
+                'iid': 'getAlpha',
+                'description': 'Alpha endpoint.',
+                'summary': 'Get request for alpha endpoint',
+                'securityDeclarationType': 'inheritedFromService',
+                'responses': [
+                  {
+                    'id': '298db75e3acd6',
+                    'code': '200',
+                    'headers': [],
+                    'contents': [
+                      {
+                        'id': 'b249580bdf6f0',
+                        'mediaType': 'application/json',
+                        'examples': [],
+                        'encodings': [],
+                        'schema': {
+                          '$schema': 'http://json-schema.org/draft-07/schema#',
+                          'type': 'object',
+                          'properties': {
+                            '_custom': {
+                              'type': 'reactive',
+                              'stateTypeName': 'Reactive',
+                              'value': {
+                                'message': {
+                                  'type': 'string',
+                                  'example': 'Alpha endpoint reached successfully.',
+                                },
+                              },
+                            },
+                          },
+                          'x-stoplight': {
+                            'id': 'f0fad1cfa9951',
+                          },
+                        },
+                      },
+                    ],
+                    'description': 'OK',
+                  },
+                ],
+                'request': {
+                  'headers': [],
+                  'query': [],
+                  'cookie': [],
+                  'path': [],
+                },
+                'security': [],
+                'servers': [
+                  {
+                    'id': '1c08f959b65b5',
+                    'url': 'https://konghq.com',
+                    'name': 'Spec with tags',
+                  },
+                ],
+              },
+              'name': 'Get request for alpha endpoint',
+              'tags': [
+                'First Tag',
+              ],
+            },
+          ],
+          'specVersion': 'OAS 3.1',
+        } as unknown as ServiceNode,
+        tableOfContents: [{ 'id': '/', 'slug': '/', 'title': 'Overview', 'type': 'overview', 'meta': '' }, { 'title': 'Endpoints', 'items': [{ 'title': 'First Tag', 'items': [{ 'id': '/operations/getAlpha', 'slug': '/operations/getAlpha', 'title': 'Get request for alpha endpoint', 'type': 'http_operation', 'meta': 'get' }], 'itemsType': 'http_operation', 'initiallyExpanded': false }], 'hideTitle': false, 'initiallyExpanded': true }],
+        currentPath: '/',
+      },
+    })
+
+    expect(wrapper.find('#tag-first-tag').exists()).toBe(true)
+  })
+
 })
