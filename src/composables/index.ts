5f041355f62676317e94e002c8deb87179dcd698
ValeryG <val.gorodnichev@konghq.com>
2025-11-20 11:57:27 -0700

    feat: adopt PR audit [KHCP-18541]

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -8,6 +8,7 @@ import useTryItState from './useTryItState'
 import useMarkdown from './useMarkdown'
 import useAuth from './useAuth'
 
+
 // All composables must be exported as part of the default object for Cypress test stubs
 export default {
   useSchemaParser,
