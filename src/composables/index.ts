2b6ea78975ea820d6008769f3b9474c98d56f037
ValeryG <val.gorodnichev@konghq.com>
2025-11-25 12:33:34 -0700

    fix: remove code changes

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -8,7 +8,6 @@ import useTryItState from './useTryItState'
 import useMarkdown from './useMarkdown'
 import useAuth from './useAuth'
 
-
 // All composables must be exported as part of the default object for Cypress test stubs
 export default {
   useSchemaParser,
